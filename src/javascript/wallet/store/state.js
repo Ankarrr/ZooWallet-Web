26024311d07dbb70eb9a654dd9d40df7b6c9b4c1
Ankarrr <twan0817@gmail.com>
2018-03-21 17:31:04 +0800

    Add web3

--- src/javascript/wallet/store/state.js
+++ src/javascript/wallet/store/state.js
@@ -1,6 +1,18 @@
+// import Web3 from "web3"
+// Note: 這裡 import 會蓋掉 browser 的 web3
+ 
+// set the provider of web3
+if (typeof web3 !== 'undefined') {
+  // web3 = new Web3(web3.currentProvider)
+  console.log("Get web3 provider")
+} else {
+  alert("No currentProvider for web3")
+}
+
 const state = {
   count: 0,
-  funcationName: ""
+  funcationName: "",
+  web3: web3
 }
 
 export {state}
