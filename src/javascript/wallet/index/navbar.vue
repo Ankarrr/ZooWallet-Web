8e0558aa93aa878fe5ec124db3eb7d195b4b4e67
Ankarrr <twan0817@gmail.com>
2018-03-22 00:46:19 +0800

    Determine and show the network of web3

--- src/javascript/wallet/index/navbar.vue
+++ src/javascript/wallet/index/navbar.vue
@@ -5,16 +5,35 @@
     <b-collapse is-nav id="nav_collapse">
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+        <b-nav-text v-if="network == 1">Mainnet</b-nav-text>
+        <b-nav-text v-else-if="network == 3">Ropsten Testnet</b-nav-text>
+        <b-nav-text v-else-if="network == 4">Rinkeby Testnet</b-nav-text>       
+        <b-nav-text v-else-if="network == 42">Kovan Testnet</b-nav-text>
+        <b-nav-text v-else-if="network == 'loading'">Private Network</b-nav-text>
+        <!--
         <b-nav-item-dropdown text="Network" right>
-          <b-dropdown-item href="#">Mainnet</b-dropdown-item>
+          <b-dropdown-item v-on:click="changeNetwork(1)">Mainnet</b-dropdown-item>
+          <b-dropdown-item v-on:click="changeNetwork(3)">Ropsten</b-dropdown-item>
         </b-nav-item-dropdown>
+        -->
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-
+export default {
+  data() {
+      return {
+          network: web3.version.network
+      }
+  },
+  methods: {
+    changeNetwork(id) {
+      console.log(id)
+    },
+  }
+}
 </script>
 
 <style>
