718a9d65ae2ac6a939e6a40a92442dcb05c659ad
Ankarrr <twan0817@gmail.com>
2018-03-23 15:41:40 +0800

    Get ETH balance in sidebar

--- src/javascript/wallet/index/sidebar.vue
+++ src/javascript/wallet/index/sidebar.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="col-2">
-
     <el-card class="box-card">
       <div>
         <h5>Your Account</h5>
-        <span class="time">{{ account }}</span>
+        <div class="account">{{ account }}</div>
+        <div class="account">{{ eth_balance }}  <span class="text-left">ETH</span></div>
       </div>
     </el-card>
     <el-menu
       @open="handleOpen"
       @close="handleClose"
-      :default-openeds="['2', '3']">      
-      <el-submenu index="2">
+      :default-openeds="['1', '2']">
+      <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-menu"></i>
           <span>Tokens</span>
         </template>
         <el-menu-item-group v-for="token in tokens">
-          <el-menu-item index="2">{{ token[1] }}  <span class="text-left">{{ token[0] }}</span></el-menu-item>
+          <el-menu-item index="">{{ token[1] }}  <span class="text-left">{{ token[0] }}</span></el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-submenu index="3">
+      <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-menu"></i>
           <span>Contracts</span>
         </template>
         <el-menu-item-group v-for="contract in contracts">
-          <el-menu-item index="3">{{ contract }}</el-menu-item>
+          <el-menu-item index="">{{ contract }}</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -38,18 +38,17 @@ import {store} from "../store/store"
 
 export default {
   data() {
-      return {
-          account: store.state.web3.eth.accounts[0],
-          // This is test Data
-          tokens: [
-              ["ETH", 2.0],
-              ["EOS", 1.2]
-          ],
-          contracts: [
-              "CryptoKitties",
-              "Bancor"
-          ]
-      }
+    return {
+        account: "",
+        eth_balance: 0,
+        tokens: [
+            ["EOS", 1.2]
+        ],
+        contracts: [
+            "CryptoKitties",
+            "Bancor"
+        ]
+    }
   },
   methods: {
     handleOpen(key, keyPath) {
@@ -57,14 +56,51 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    setAccount() {
+      if (store.state.web3.eth.accounts[0] === undefined) {
+        this.$alert('Please unlock your MetaMask.', 'Notice', {
+          confirmButtonText: 'Enter',
+          callback: action => {
+            // this.$message({
+            //   type: 'info',
+            //   message: `action: ${ action }`
+            // });
+          }
+        })
+      } else {
+        this.account = store.state.web3.eth.accounts[0]
+        store.commit("updateAccount")
+      }
+    },
+    getEthBalance(account) {
+      let self = this
+      store.state.web3.eth.getBalance(account, function(error, result){
+        if (error) {
+          console.debug(error)
+          return
+        }
+        self.eth_balance = result.div(1000000000000000000).toFixed(4,1)
+        console.debug("Get ETH balance " + self.eth_balance)
+      })
+    }
+  },
+  mounted() {
+    this.setAccount()
+
+  },
+  watch: {
+    account() {
+      this.getEthBalance(this.account)
     }
   }
 }
 </script>
 
 <style>
-  .time {
+  .account {
     font-size: 14px;
     color: #999;
+    padding-bottom: 5px;
   }
 </style>
