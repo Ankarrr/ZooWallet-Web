d7c7349d58a06665885abd353fa8726b976ae278
Ankarrr <twan0817@gmail.com>
2018-03-30 13:19:51 +0800

    Detect Ethereum network when "computed"

--- src/javascript/wallet/components/navbar.vue
+++ src/javascript/wallet/components/navbar.vue
@@ -5,11 +5,7 @@
     <b-collapse is-nav id="nav_collapse">
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-text v-if="network == 1">Mainnet</b-nav-text>
-        <b-nav-text v-else-if="network == 3">Ropsten Testnet</b-nav-text>
-        <b-nav-text v-else-if="network == 4">Rinkeby Testnet</b-nav-text>       
-        <b-nav-text v-else-if="network == 42">Kovan Testnet</b-nav-text>
-        <b-nav-text v-else-if="network == 'loading'">Private Network</b-nav-text>
+        <b-nav-text v-if="isConnected === true">{{ network }}</b-nav-text>
         <!--
         <b-nav-item-dropdown text="Network" right>
           <b-dropdown-item v-on:click="changeNetwork(1)">Mainnet</b-dropdown-item>
@@ -25,15 +21,29 @@
 import {store} from "../store/store"
 
 export default {
-  data() {
-      return {
-          network: store.state.web3.version.network
-      }
-  },
   methods: {
     changeNetwork(id) {
       console.log(id)
     },
+  },
+  computed: {
+    network() {
+      let networkID = store.state.web3.version.network
+      if (networkID == 1) {
+        return 'Mainnet'
+      } else if (networkID == 3) {
+        return 'Ropsten Testnet'
+      } else if (networkID == 4) {
+        return 'Rinkeby Testnet'
+      } else if (networkID == 42) {
+        return 'Kovan Testnet'
+      } else {
+        return 'Private Network'
+      }
+    },
+    isConnected() {
+      return store.state.web3.isConnected()
+    }
   }
 }
 </script>
