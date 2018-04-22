5ba381a89d11c912054aae30d29e1c3678f8c974
Perrychen25 <perrychen25@gmail.com>
2018-04-22 21:02:50 +0800

    add transcations list href of from and to is still not finished yet.

--- src/javascript/wallet/components/app.vue
+++ src/javascript/wallet/components/app.vue
@@ -2,10 +2,9 @@
   <div class="wallet_body col-10">
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="Send" name="first">Send</el-tab-pane>
-      <el-tab-pane label="Transactions" name="second">Transactions</el-tab-pane>
+      <el-tab-pane label="Transactions" name="second" ><transactions ref="abc" /></el-tab-pane>
       <el-tab-pane label="Contract" name="third"><contract></contract></el-tab-pane>
-      <el-tab-pane label="Market" name="fourth">Market information</el-tab-pane>
-      <el-tab-pane label="Exchange" name="fifth">Exchange</el-tab-pane>
+      <el-tab-pane label="Exchange" name="fourth">exchange</el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -15,24 +14,23 @@ import ifetch from "jsappbase/ifetch"
 import _ from "lodash"
 import Cookies from 'js-cookie'
 import Contract from './contract.vue'
+import Transactions from './transactions.vue'
+
 
 export default {
-  components: {Contract},
+  components: {
+    Contract,
+    Transactions,
+  },
   data() {
     return {
-      // activeName decides which tab is open
       activeName: 'third'
-    }
+    };
   },
   methods: {
     handleClick(tab, event) {
-      this.$emit("on-tab-change", this.activeName)
-    },
-  },
-  props: ['newActiveName'],
-  watch: {
-    newActiveName() {
-      this.activeName = this.newActiveName
+      console.log(tab, event);
+      this.$refs.abc.created();
     }
   }
 }
